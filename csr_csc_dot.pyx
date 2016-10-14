c2e4a15f28a8ab524f159407708929c69dfac972
Filip Ginter <ginter@cs.utu.fi>
2016-10-14 16:44:11 +0300

    Values now work, and also checking for dimensions. Fixed slice adjustment.

--- csr_csc_dot.pyx
+++ csr_csc_dot.pyx
@@ -7,7 +7,7 @@ import sys
 ctypedef np.int32_t idx_type;
 ctypedef np.float32_t val_type;
 
-cdef extern void fast_dot(idx_type m1_start, idx_type m1_slice_len, idx_type *m1_row_ptr, idx_type *m1_indices, idx_type *m2_col_ptr, idx_type *m2_indices, val_type *out, idx_type out_col_number)
+cdef extern void fast_dot(idx_type m1_start, idx_type m1_slice_len, idx_type *m1_row_ptr, idx_type *m1_indices, val_type *m1_data, idx_type *m2_col_ptr, idx_type *m2_indices, val_type *m2_data, val_type *out, idx_type out_col_number)
 
 #m1 is CSR
 #m2 is CSC
@@ -17,9 +17,14 @@ cdef extern void fast_dot(idx_type m1_start, idx_type m1_slice_len, idx_type *m1
 
 def csr_csc_dot_f(start,slice,m1,m2, np.ndarray[np.float32_t,ndim=2,mode="c"] out):
 
-    if start+slice > out.shape[0]:
-       print("csr_csc_dot: adjusting slice to ",out.shape[0]-start,file=sys.stderr)
-       slice=out.shape[0]-start
+    if (m1.shape[1]!=m2.shape[1]):
+       raise ValueError("M1 shape[1]={} does not match M2 shape[1]={}".format(m1.shape[1],m2.shape[1]))
+    if (m1.shape[0]!=out.shape[0] and m2.shape[0]!=out.shape[1]):
+       raise ValueError("M1 shape[0]={} should match out shape[0]={} and M2 shape[0]={} should match out shape[1]={}".format(m1.shape[0],out.shape[0],m2.shape[0],out.shape[1]))
+       
+    if start+slice > m1.shape[0]:
+       print("csr_csc_dot: adjusting slice to ",m1.shape[0]-start,file=sys.stderr)
+       slice=m1.shape[0]-start
 
     cdef np.ndarray[np.int32_t,ndim=1,mode="c"] m1_i=m1.indices
     cdef idx_type * m1_idx=<np.int32_t *> m1_i.data
@@ -27,14 +32,21 @@ def csr_csc_dot_f(start,slice,m1,m2, np.ndarray[np.float32_t,ndim=2,mode="c"] ou
     cdef np.ndarray[np.int32_t,ndim=1,mode="c"] m1_p=m1.indptr
     cdef idx_type * m1_ptr=<np.int32_t *> m1_p.data
 
+    cdef np.ndarray[np.float32_t,ndim=1,mode="c"] m1_d=m1.data
+    cdef val_type * m1_data=<np.float32_t *> m1_d.data
+
     cdef np.ndarray[np.int32_t,ndim=1,mode="c"] m2_i=m2.indices
     cdef idx_type * m2_idx=<np.int32_t *> m2_i.data
 
     cdef np.ndarray[np.int32_t,ndim=1,mode="c"] m2_p=m2.indptr
     cdef idx_type * m2_ptr=<np.int32_t *> m2_p.data
 
+    cdef np.ndarray[np.float32_t,ndim=1,mode="c"] m2_d=m2.data
+    cdef val_type * m2_data=<np.float32_t *> m2_d.data
+
+
     cdef val_type * out_data=<np.float32_t *> out.data
 
     out.fill(0.0)
-    fast_dot(start,slice,m1_ptr,m1_idx,m2_ptr,m2_idx,out_data,out.shape[1])
+    fast_dot(start,slice,m1_ptr,m1_idx,m1_data,m2_ptr,m2_idx,m2_data,out_data,out.shape[1])
 
--- csr_csc_multiply.c
+++ csr_csc_multiply.c
@@ -17,19 +17,22 @@ indptr [0 0 0 ..., 3 3 5]
 typedef int32_t idx_type;
 typedef float val_type;
 
-void fast_dot(idx_type m1_start, idx_type m1_slice, idx_type *m1_row_ptr, idx_type *m1_indices, idx_type *m2_col_ptr, idx_type *m2_indices, val_type *out, idx_type out_col_number) {
+void fast_dot(idx_type m1_start, idx_type m1_slice, idx_type *m1_row_ptr, idx_type *m1_indices, val_type *m1_data, idx_type *m2_col_ptr, idx_type *m2_indices, val_type *m2_data, val_type *out, idx_type out_col_number) {
     idx_type out_row,col,out_col,m1_ri,m1_ci,m2_ri;
-
+    val_type m1_val,m2_val;
+    
     for (m1_ri=m1_start; m1_ri<m1_start+m1_slice; m1_ri++) { //for every row of m1
 	out_row=m1_ri-m1_start;
 	
 	for (m1_ci=m1_row_ptr[m1_ri]; m1_ci<m1_row_ptr[m1_ri+1]; m1_ci++) { //for every column on that row
 	    col=m1_indices[m1_ci]; //this is now a column index in m2
+	    m1_val=m1_data[m1_ci]; //the corresponding value from m1
 
 	    for (m2_ri=m2_col_ptr[col]; m2_ri<m2_col_ptr[col+1]; m2_ri++) { //for every row in m2 in this column
 		out_col=m2_indices[m2_ri];
+		m2_val=m2_data[m2_ri];
 
-		out[out_col_number*out_row+out_col]+=1.0;
+		out[out_col_number*out_row+out_col]+=m1_val*m2_val;
 	    }
 	}
     }//m1 row index loop
